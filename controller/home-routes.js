2eb08491713f577aff09e0fe809ef843e94df527
Dustin Grijalva <dustin2400@hotmail.com>
2021-12-24 23:22:14 -0600

    added post page

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -79,4 +79,41 @@ router.get('/edit/:id', (req, res) => {
     });
 });
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Comment,
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+    
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
