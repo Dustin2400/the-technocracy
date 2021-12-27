4544484956754266c15ecc421598f97f25f25817
Dustin Grijalva <dustin2400@hotmail.com>
2021-12-26 23:53:51 -0600

    troubleshooting heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const routes = require('./controller');
+const sequelize = require('./config/connection');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
-const path = require('path');
-const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -18,7 +20,7 @@ const sess = {
         db: sequelize
     })
 };
-const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 
